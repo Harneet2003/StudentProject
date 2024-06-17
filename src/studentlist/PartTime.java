21d8d617ddd4c6ad21241290b1599e3c429d0407
harneet2003 <kau16367@sheridancollege.ca>
2024-06-17 00:04:56 -0400

    mid term

---
+++ src/studentlist/PartTime.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author User
+ */
+public class PartTime extends Student {
+    
+    private int hrs;
+
+    public int getHrs() {
+        return hrs;
+    }
+
+    public void setHrs(int hrs) {
+        this.hrs = hrs;
+    }
+    
+    
+}
